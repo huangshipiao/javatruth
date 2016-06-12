d52a6acbeb681b35cb6979f2b4b8c0d5bf2910c0
huangshipiao <517557384@qq.com>
2016-06-12 23:50:03 +0800

    新增toastr.js插件

--- javatruth/src/main/java/com/javatruth/action/admin/AdminIndexAct.java
+++ javatruth/src/main/java/com/javatruth/action/admin/AdminIndexAct.java
@@ -2,12 +2,15 @@ package com.javatruth.action.admin;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.common.utils.RequestUtils;
+import com.common.utils.StringUtils;
 import com.common.web.WebSite;
 import com.javatruth.service.IUserService;
 
@@ -20,20 +23,33 @@ public class AdminIndexAct {
 	
 	
 	@RequestMapping("index.html")
-	public String adminIndex(HttpServletRequest request,Model model){		
+	public String adminIndex(HttpServletRequest request,ModelMap model){		
 		return WebSite.getAdminTemplate("index");
 	}
 	
 	
 	@RequestMapping(value = "login.html", method = RequestMethod.GET)
-	public String toLogin(HttpServletRequest request,Model model){		
+	public String toLogin(HttpServletRequest request,ModelMap model){		
 		return WebSite.getAdminTemplate("login");
 	}
 	
 	
-	@RequestMapping(value = "login.do", method = RequestMethod.POST)
-	public String login(HttpServletRequest request,Model model){		
-		
+	@RequestMapping(value = "login.do", method = {RequestMethod.POST,RequestMethod.GET})
+	public String login(String username,String password,HttpServletRequest request,HttpServletResponse response,ModelMap model){		
+		WebSite.setParameters(request, model);
+		System.out.println("username:"+username+",password:"+password);
+		if(StringUtils.isBlank(username)){
+			model.addAttribute("message", "用户名不能为空.");
+			return WebSite.getAdminTemplate("login");
+		}
+		if(StringUtils.isBlank(password)){
+			model.addAttribute("message", "密码不能为空.");
+			return WebSite.getAdminTemplate("login");
+		}
+//		userService.findByUserName(username);
+		model.addAttribute("message", "登录成功");
 		return WebSite.getAdminTemplate("login");
 	}
+	
+	
 }
